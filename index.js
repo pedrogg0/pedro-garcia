14d91e5396da25f71952a50d4300da019513de5a
pedrogg0 <pedrojosegarcia00@gmail.com>
2023-09-22 09:02:38 +0200

    added Animation to Projects page

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $("#simon-game").click(function(){
     open("https://pedrogg0.github.io/simon-game/");
 });
+
+$("#simon-game").mouseover(function () { 
+    $("#simon-game").addClass("mouse-over");
+});
+
+$("#simon-game").mouseout(function () { 
+    $("#simon-game").removeClass("mouse-over");
+});
