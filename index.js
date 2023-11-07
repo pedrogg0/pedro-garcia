b6f25feeb3f8b43db4beff1594319643e26ab078
pedrogg0 <pedrojosegarcia00@gmail.com>
2023-11-07 11:49:37 +0100

    added flappy-hacker project

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ $("#simon-game").mouseover(function () {
 $("#simon-game").mouseout(function () { 
     $("#simon-game").removeClass("mouse-over");
 });
+
+$("#flappy-hacker").click(function(){
+    open("https://pedrogg0.github.io/flappy-hacker/");
+});
+
+$("#flappy-hacker").mouseover(function () { 
+    $("#flappy-hacker").addClass("mouse-over");
+});
+
+$("#flappy-hacker").mouseout(function () { 
+    $("#flappy-hacker").removeClass("mouse-over");
+});
